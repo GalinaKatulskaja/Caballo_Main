247e9ed302cd16d7ba5622b3e7d15e72c5f5c786
Galina K. <galinakatulskaja1228@list.ru>
2018-11-12 16:34:30 +0100

    4) Creamos ENUM con Direcciones ARRIBA_IZQUIERDA, ARRIBA_DERECHA, DERECHA_ARRIBA, DERECHA_ABAJO, ABAJO_DERECHA, ABAJO_IZQUIERDA, IZQUIERDA_ARRIBA y IZQUIERDA_ABAJO

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Direccion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ *
+ * @author Galina
+ * 
+ * Creamos posibles direcciones que puede tomar un caballo de ajedrez
+ * 
+ * ARRIBA_DERECHA
+ * ARRIBA_IZQUIERDA
+ * DERECHA_ARRIBA
+ * DERECHA_ABAJO
+ * ABAJO_DERECHA
+ * ABAJO_IZQUIERDA
+ * IZQUIERDA_ARRIBA
+ * IZQUIERDA_ABAJO
+ */
+public enum Direccion {
+    
+    ARRIBA_IZQUIERDA, ARRIBA_DERECHA, DERECHA_ARRIBA, DERECHA_ABAJO, ABAJO_DERECHA, ABAJO_IZQUIERDA, IZQUIERDA_ARRIBA, IZQUIERDA_ABAJO; 
+}

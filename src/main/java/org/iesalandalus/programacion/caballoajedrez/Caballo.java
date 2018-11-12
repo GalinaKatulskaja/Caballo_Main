b4f13fadb5bfd89d05ddb01b2a17644226151b93
Galina K. <galinakatulskaja1228@list.ru>
2018-11-12 18:07:48 +0100

    11) Creamos la clase Caballo, cuyos atributos son; color (del tipo enumerado Color)y posicion (de la clase Posicion)

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ *
+ * @author Galina
+ */
+public class Caballo {
+    
+     /* Creamos la clase Caballo, cuyos atributos son; color (del tipo enumerado Color)
+    y posicion (de la clase Posicion)*/
+    
+    private Color color;
+    private Posicion posicion;
+    
+}

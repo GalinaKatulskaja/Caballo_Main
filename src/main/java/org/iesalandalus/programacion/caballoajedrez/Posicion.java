03f7624cc72623b7e3bf43197e572bf8c87f035a
Galina K. <galinakatulskaja1228@list.ru>
2018-11-12 16:38:01 +0100

    5) Crea la clase Posicion. Crea los atributos fila (int) y columna (char)

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ *
+ * @author Galina
+ * 
+ * //Crea la clase Posicion. Crea los atributos fila (int) y columna (char)
+ */
+public class Posicion {
+    private int fila;
+    private char columna;
+    
+}

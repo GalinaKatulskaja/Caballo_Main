110f7b373e6504a71917c8e04787d428cd547357
Galina K. <galinakatulskaja1228@list.ru>
2018-11-12 18:11:29 +0100

    13) Creamos un constructor por defecto para la clase Caballo que cree un caballo negro en la posición '8b'

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -17,6 +17,13 @@ public class Caballo {
     private Color color;
     private Posicion posicion;
     
+    //Creamos un constructor por defecto para la clase Caballo que cree un caballo negro en la posición '8b'
+    public Caballo()
+    {
+        color = Color.NEGRO;
+        posicion = new Posicion(8,'b');
+    }
+    
     //Creamos los métodos get y set para cada atributo
 
     public Color getColor() {

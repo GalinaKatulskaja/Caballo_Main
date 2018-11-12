ccc853df118cbc9a55cb1aa112641782fb851459
Galina K. <galinakatulskaja1228@list.ru>
2018-11-12 16:43:12 +0100

    6) Creamos los métodos get y set para los atributos:(las filas van del 1 al 8 -ambos inclusive- y las columnas de la 'a' a la 'h'-ambos inclusive-)en caso contrario se lanza una excepción (IllegalArgumentException) con el mensaje.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -15,4 +15,39 @@ public class Posicion {
     private int fila;
     private char columna;
     
+    
+    /*Creamos los métodos get y set para los atributos:
+    (las filas van del 1 al 8 -ambos inclusive- y las columnas 
+    de la 'a' a la 'h' -ambos inclusive-)  y si no se lance 
+    una excepción del tipo IllegalArgumentException con el mensaje. */
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if(fila<1 | fila>8)
+        {
+            throw new IllegalArgumentException ("ERROR - La fila introducida no es correcta");
+        }
+        else
+        {
+        this.fila = fila;
+        }
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if(columna!='a'& columna!='b' & columna!='c'& columna!='d' & columna!='e' & columna!='f' & columna!='g' & columna!='h')
+        {
+            throw new IllegalArgumentException ("ERROR - La columna introducida no es correcta");
+        }
+        else
+        {
+        this.columna = columna;
+        }
+    }
+    
 }
